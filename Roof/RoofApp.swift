42a30fa2ea711f12034835701f91022afabb301e
Rob Farley <farleyr2538@icloud.com>
2024-02-29 08:37:40 +0000

    Initial Commit

---
+++ Roof/RoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoofApp.swift
+//  Roof
+//
+//  Created by Robert Farley on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
